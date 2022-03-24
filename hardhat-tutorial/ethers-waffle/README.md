ca2f255472f7f35a9cefbc8fec96a5004bfb4467
Aleksandr Shenshin <shenshin@me.com>
2022-03-25 00:43:01 +0100

    add ethers-waffle issues readme

---
+++ hardhat-tutorial/ethers-waffle/README.md
@@ -0,0 +1,42 @@
+I encounter 2 problems while testing smart contracts with Hardhat
+
+1. I can't send transactions via ether.js to a function of a smart contract deployed on RSK regtest. However the same transactions work on ganache or hardhat test networks. 
+2. Mnemonic phrase produces "wrong" address 
+
+To reproduce:
+
+## Setup up environment (mac):
+```bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | bash
+nvm install 12
+nvm use 12
+nvm alias default 12
+npm install npm --global # Upgrade npm to the latest version
+```
+## Hardhat and project setup
+```bash
+npm install
+```
+## Runn Hardhat tests
+On Hardhat network:
+
+```bash
+npm run hardhat
+```
+Tests pass.
+
+On RSK regtest network:
+
+```bash
+npm run regtest
+```
+Tests fail.
+
+If I try to run the same test on RSK testnet:
+
+```bash
+npm run testnet
+```
+Hardhat generates wrong addresses from the mnemonic, and the test fails
+with an error saing 
+`ProviderError: the sender account doesn't exist`
--- hardhat-tutorial/ethers-waffle/test/Token.js
+++ hardhat-tutorial/ethers-waffle/test/Token.js
@@ -1,25 +1,31 @@
-const { expect } = require("chai");
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+const { expect } = require('chai');
 
-describe("Transactions", function() {
-  it("Should transfer tokens between accounts", async function() {
+describe('Transactions', () => {
+  it('Should transfer tokens between accounts', async () => {
     const signers = await ethers.getSigners();
+    // convert addresses to lower case
     signers.forEach((signer) => {
-      signer.address = signer.address.toLowerCase()
+      // eslint-disable-next-line no-param-reassign
+      signer.address = signer.address.toLowerCase();
     });
     const [owner, addr1, addr2] = signers;
-    const Token = await ethers.getContractFactory("Token");
-    console.log(owner.address, addr1.address);
+    const Token = await ethers.getContractFactory('Token');
+    console.log('Owner account: ', owner.address);
     const hardhatToken = await Token.deploy();
-    console.log(await hardhatToken.balanceOf(owner.address));
-    console.log(await hardhatToken.balanceOf(addr1.address));
+    console.log('Before transction');
+    console.log('Owner balance: ', await hardhatToken.balanceOf(owner.address));
+    console.log('Addr1 balance: ', await hardhatToken.balanceOf(addr1.address));
     // Transfer 50 tokens from owner to addr1
     await hardhatToken.connect(owner).transfer(addr1.address, 50);
-    console.log(await hardhatToken.balanceOf(owner.address));
-    console.log(await hardhatToken.balanceOf(addr1.address));
-    // expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+    console.log('After transction');
+    console.log('Owner balance: ', await hardhatToken.balanceOf(owner.address));
+    console.log('Addr1 balance: ', await hardhatToken.balanceOf(addr1.address));
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
 
     // Transfer 50 tokens from addr1 to addr2
-    /* await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50); */
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
   });
 });

847eed47b775f09ebe1a2e992a09479780f24a8e
Aleksandr Shenshin <shenshin@me.com>
2022-09-11 21:19:14 +0200

    demo repo showing how to query the number of smart contracts deployed to RSK Testnet in the last week

---
+++ rsk-deployments-ma/dbPool.js
@@ -0,0 +1,13 @@
+const pg = require('pg');
+require('dotenv').config();
+
+const dbPool = new pg.Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: true,
+});
+
+module.exports = dbPool;

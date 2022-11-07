da43dc313725eb22acdf9931d31da668816d52a2
Aleksandr Shenshin <shenshin@me.com>
2022-11-07 16:24:16 +0100

    fix typos

--- multicall-0xsequence/multicall-config.js
+++ multicall-0xsequence/multicall-config.js
@@ -6,7 +6,7 @@ const multicallConfig = {
     // defines the time each call is held on buffer waiting for subsequent calls before aggregation, ms
     timeWindow: 50,
     /* 
-    0xsequience MultiCall
+    0xsequence MultiCall
     https://github.com/0xsequence/wallet-contracts/blob/master/src/contracts/modules/utils/MultiCallUtils.sol
     deployed at RSK Testnet
     https://explorer.testnet.rsk.co/address/0xb39d1dea1bf91aef02484f677925904b9d6936b4?__ctab=Code

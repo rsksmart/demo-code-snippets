c50c456f392225254b7a56c0fd04b611e5d8eba8
Aleksandr Shenshin <shenshin@me.com>
2023-01-19 13:54:09 +0100

    convert bytecode to a string of opcodes

--- bytecode-to-opcodes/test/basicToken.test.ts
+++ bytecode-to-opcodes/test/basicToken.test.ts
@@ -1,21 +1,9 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
-import { BasicToken__factory } from '../typechain-types';
-import { disassemble, Bytecode } from '@ethersproject/asm';
+import { getBytecode, getOpcodes } from '../opcodes';
 
 describe('Basic token', () => {
-  let bytecode: Bytecode;
-
-  before(async () => {
-    const BasicTokenFactory: BasicToken__factory =
-      await ethers.getContractFactory('BasicToken');
-    const bytecodeString = BasicTokenFactory.bytecode;
-    bytecode = disassemble(bytecodeString);
-  });
-
-  // opcodes list and description https://ethervm.io/#opcodes
-
   it('Opcode 000 should be PUSH1 80', async () => {
+    const bytecode = await getBytecode('BasicToken');
     const operation = bytecode[0];
     expect(operation.offset).to.equal(0); // line number
     expect(operation.opcode.mnemonic).to.equal('PUSH1'); // mnemonic name
@@ -24,6 +12,7 @@ describe('Basic token', () => {
   });
 
   it('Opcode 002 should be PUSH1 40', async () => {
+    const bytecode = await getBytecode('BasicToken');
     const operation = bytecode[1];
     expect(operation.offset).to.equal(2); // line number
     expect(operation.opcode.mnemonic).to.equal('PUSH1'); // mnemonic name
@@ -32,9 +21,19 @@ describe('Basic token', () => {
   });
 
   it('Opcode 004 should be MSTORE', async () => {
+    const bytecode = await getBytecode('BasicToken');
     const operation = bytecode[2];
     expect(operation.offset).to.equal(4); // line number
     expect(operation.length).to.equal(1); // 1 byte
     expect(operation.opcode.mnemonic).to.equal('MSTORE'); // mnemonic name
+  });
+
+  it('Should generate a string with all opcodes', async () => {
+    const bytecode = await getBytecode('BasicToken');
+    const opcodes: string = getOpcodes(bytecode);
+    const instructions = opcodes.split(', ');
+    expect(instructions[0]).to.equal('PUSH1 0x80');
+    expect(instructions[1]).to.equal('PUSH1 0x40');
+    expect(instructions[2]).to.equal('MSTORE');
   });
 });
